b248bb2767b119642b42f909957a036d3d11ecb2
TheGrayback <nchmil18@gmail.com>
2025-10-03 19:13:38 +0300

    refactor: change product type Product to satisfy product.json structure

--- src/types/product.types.ts
+++ src/types/product.types.ts
@@ -1,11 +1,10 @@
 export type Product = {
-    id: number;
-    name: string;
-    price: number;
-    imageUrl: string;
-    color: Array<Array<string>>;
-    fullDescription: string;
-    shortDescription: string;
-    sizes: Object;
-
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  color: [string, string][];
+  shortDescription: string;
+  fullDescription: string;
+  sizes: string[];
 };
