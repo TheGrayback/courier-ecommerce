8ec18db4f82113d58a58fde720dea71977f024d4
TheGrayback <nchmil18@gmail.com>
2025-10-03 19:14:56 +0300

    chore: import type Product instead of having in hardcoded into MainApp.tsx (should've done this before)

--- src/components/MainPage/Grid/MainPage.tsx
+++ src/components/MainPage/Grid/MainPage.tsx
@@ -1,13 +1,7 @@
 import GridItem from '../GridItem/GridItem';
 import styles from './MainPage.module.css'
 import data from '../../../utils/products.json';
-
-type Product = {
-    id: number;
-    name: string;
-    price: number;
-    imageUrl: string;
-};
+import type { Product } from '@/types/product.types';
 
 const products: Product[] = data;
 
